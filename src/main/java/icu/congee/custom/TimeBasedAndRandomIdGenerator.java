5743503e2c64e1fd12aaaab14961ff0b6c591a81
熊迪 <ixiongdi@gmail.com>
2025-03-04 16:45:22 +0800

    初始化

--- src/main/java/icu/congee/custom/TimeBasedAndRandomIdGenerator.java
+++ src/main/java/icu/congee/custom/TimeBasedAndRandomIdGenerator.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class TimeBasedAndRandomIdGenerator implements NumberIdGenerator {
 
     /**
-     * 2024-05-01 00:00:00 纪念RFC9652标准发布
+     * Wed May 01 2024 00:00:00 GMT+0800 (GMT+08:00) 纪念RFC9652标准发布
      */
     private static final long EPOCH = 1714492800L;
 
