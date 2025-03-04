5755f7bcec4b634121cec7867cf43623812ccda2
熊迪 <ixiongdi@gmail.com>
2025-03-04 16:43:17 +0800

    初始化

--- src/main/java/icu/congee/Main.java
+++ src/main/java/icu/congee/Main.java
@@ -1,34 +1,17 @@
 package icu.congee;
 
 import cn.hutool.log.Log;
-import icu.congee.LexicalUUID.LexicalUUID;
-import icu.congee.LexicalUUID.MicrosecondEpochClock;
-import icu.congee.cuid.CUID;
-import icu.congee.ulid.ULID;
-
 
 public class Main {
     private static final Log log = Log.get();
 
     public static void main(String[] args) {
 
-        /**
-         * import de.huxhorn.sulky.ulid.ULID;
-         *
-         * ULID ulid = new ULID();
-         */
-        ULID ulid = new ULID();
-        log.info("ulid={}", ulid.nextULID());
-        for(int i = 0; i < 10; i++) {
-            LexicalUUID lexicalUUID = LexicalUUID.create(MicrosecondEpochClock.getInstance());
-            log.info("lexicalUUID={}", lexicalUUID);
+        for (int i = 0; i < 10; i++) {
+            log.info("timeBasedBusinessId={}", BestPracticeNumberIdGenerator.timeBasedBusinessId());
         }
-
-        for(int i = 0; i < 10; i++) {
-            log.info("cuid1={}", CUID.randomCUID1());
-        }
-        for(int i = 0; i < 10; i++) {
-            log.info("cuid2={}", CUID.randomCUID2());
+        for (int i = 0; i < 10; i++) {
+            log.info("timeBasedRandomId={}", BestPracticeNumberIdGenerator.timeBasedRandomId());
         }
     }
 }
--- src/main/java/icu/congee/custom/TimeBasedAndRandomIdGenerator.java
+++ src/main/java/icu/congee/custom/TimeBasedAndRandomIdGenerator.java
@@ -7,8 +7,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class TimeBasedAndRandomIdGenerator implements NumberIdGenerator {
 
+    /**
+     * 2024-05-01 00:00:00 纪念RFC9652标准发布
+     */
+    private static final long EPOCH = 1704038400L;
+
     public static long next() {
-        return (Instant.now().getEpochSecond() << 32) | ThreadLocalRandom.current().nextInt();
+        return (Instant.now().getEpochSecond() - EPOCH << 32)
+                | (ThreadLocalRandom.current().nextInt() & 0xFFFFFFFFL);
     }
 
     @Override
