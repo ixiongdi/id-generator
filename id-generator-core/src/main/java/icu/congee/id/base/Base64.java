976a40819aee42d6a70bcdf977246c02ebef8841
熊迪 <ixiongdi@gmail.com>
2025-04-16 19:32:08 +0800

    发布0.4.9

--- id-generator-core/src/main/java/icu/congee/id/base/Base64.java
+++ id-generator-core/src/main/java/icu/congee/id/base/Base64.java
@@ -7,10 +7,13 @@ import java.util.Arrays;
  */
 public class Base64 {
 
-    private static final char[] URL_SAFE_CHARS = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
-            'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    private static final char[] URL_SAFE_CHARS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z',
+            '_',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z',
+            '~' };
 
     private static final byte[] DECODE_TABLE = new byte[128];
 
