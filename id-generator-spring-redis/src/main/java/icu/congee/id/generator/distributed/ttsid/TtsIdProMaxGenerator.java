78924ab51e47ec3562403558e385dbea1b94fce3
熊迪 <ixiongdi@gmail.com>
2025-06-10 18:36:50 +0800

    发布0.6.2

--- id-generator-spring-redis/src/main/java/icu/congee/id/generator/distributed/ttsid/TtsIdProMaxGenerator.java
+++ id-generator-spring-redis/src/main/java/icu/congee/id/generator/distributed/ttsid/TtsIdProMaxGenerator.java
@@ -2,7 +2,9 @@ package icu.congee.id.generator.distributed.ttsid;
 
 import icu.congee.id.base.IdGenerator;
 import icu.congee.id.base.IdType;
+
 import lombok.AllArgsConstructor;
+
 import org.redisson.api.RAtomicLong;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
@@ -13,14 +15,29 @@ public class TtsIdProMaxGenerator implements IdGenerator {
     private final ThreadLocal<TtsIdProMaxThreadLocalHolder> threadLocalHolder;
 
     public TtsIdProMaxGenerator(RedissonClient redisson) {
-        RAtomicLong threadId = redisson.getAtomicLong("IdGenerator:TtsIdProMaxGenerator:threadId");
-
-        threadLocalHolder = ThreadLocal.withInitial(() -> new TtsIdProMaxThreadLocalHolder((int) threadId.getAndIncrement(), 0));
+        RAtomicLong atomicLong =
+                redisson.getAtomicLong("IdGenerator:TtsIdProMaxGenerator:threadId");
+        if (!atomicLong.isExists()) {
+            atomicLong.set(0);
+        }
+        threadLocalHolder =
+                ThreadLocal.withInitial(
+                        () -> {
+                            long threadId = atomicLong.getAndIncrement();
+                            if (threadId >= Integer.MAX_VALUE) {
+                                atomicLong.set(0);
+                            }
+                            return new TtsIdProMaxThreadLocalHolder((int) threadId, 0);
+                        });
     }
 
     @Override
     public TtsIdProMax generate() {
         TtsIdProMaxThreadLocalHolder holder = threadLocalHolder.get();
+        int sequence = holder.sequence;
+        if (sequence == Integer.MAX_VALUE) {
+            holder.sequence = 0;
+        }
         return new TtsIdProMax(TtsIdProMax.currentTimestamp(), holder.threadId, holder.sequence++);
     }
 
