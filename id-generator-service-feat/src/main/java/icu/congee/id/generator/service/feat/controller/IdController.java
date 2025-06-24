2ee9f250988f563356bf6b18593f25a910914f60
熊迪 <ixiongdi@gmail.com>
2025-06-24 16:42:34 +0800

    发布0.6.2

--- id-generator-service-feat/src/main/java/icu/congee/id/generator/service/feat/controller/IdController.java
+++ id-generator-service-feat/src/main/java/icu/congee/id/generator/service/feat/controller/IdController.java
@@ -1,17 +1,11 @@
 package icu.congee.id.generator.service.feat.controller;
 
 import icu.congee.id.util.IdUtil;
-
-import tech.smartboot.feat.cloud.FeatCloud;
 import tech.smartboot.feat.cloud.annotation.Controller;
 import tech.smartboot.feat.cloud.annotation.RequestMapping;
 
 @Controller
 public class IdController {
-    public static void main(String[] args) {
-        FeatCloud.cloudServer().listen();
-    }
-
     @RequestMapping("/id/generate")
     public String generate() {
         return IdUtil.uuid7();
